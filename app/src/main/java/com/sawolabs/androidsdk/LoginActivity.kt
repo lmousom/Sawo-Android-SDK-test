ab8ef598118f5fb86fb2cc6ff1746b990b362daa
mousom <latifulmousom@gmail..com>
2021-12-02 11:12:25 +0530

    delay fix check

--- app/src/main/java/com/sawolabs/androidsdk/LoginActivity.kt
+++ app/src/main/java/com/sawolabs/androidsdk/LoginActivity.kt
@@ -129,7 +129,6 @@ class LoginActivity : AppCompatActivity(), OSSubscriptionObserver {
 
         lifecycleScope.launch {
             mWebView.loadUrl(sawoWebSDKURL)
-            delay(2000L)
             val sharedPref = getSharedPreferences(SHARED_PREF_FILENAME, Context.MODE_PRIVATE)
             mWebView.addJavascriptInterface(
                 SawoWebSDKInterface(
@@ -143,8 +142,8 @@ class LoginActivity : AppCompatActivity(), OSSubscriptionObserver {
 //            if (0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) {
 //                WebView.setWebContentsDebuggingEnabled(true)
 //            }
-
-
+            delay(2000L)
+            mWebView.loadUrl(sawoWebSDKURL)
         }
 //        Handler(Looper.getMainLooper()).postDelayed(
 //            {
